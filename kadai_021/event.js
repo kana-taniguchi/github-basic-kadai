6ebfe68516b59e0d3c1ccc50565c30a1b9a16287
kana-taniguchi <165878685+kana-taniguchi@users.noreply.github.com>
2024-04-11 13:55:09 +0900

    kadai_021を提出

---
+++ kadai_021/event.js
@@ -0,0 +1,11 @@
+const addBtn = document.getElementById('btn');
+
+const parentList = document.getElementById('text');
+
+addBtn.addEventListener('click', () => {
+    setTimeout(() => {
+    //li要素を新しく追加する
+    text.textContent = 'ボタンをクリックしました';
+    }, 2000);
+
+});
